78f3a5057d330e859890ce59014a91fbe519f317
soumya <197779975+git-by-soumya@users.noreply.github.com>
2025-02-28 04:03:09 +0530

    Add function getComputerChoice to javascript file

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Hello World");
+function getComputerChoice() {
+    /*
+    Plan:
+    UI: console (F12 -> Console)
+    Input: none
+    Output: randomly return one of the following string values - "rock", "paper" or "scissors"
+    Pseudocode:
+    */
+
+    //Initialize a variable randomResult to 0, to later hold a random number between 0 and 9, inclusive.
+    let randomResult = 0;
+    //Get a random number between 0 and 1, multiply it with 10, and round it down to remove the decimal part.
+    //Assign the above value to randomResult.
+    randomResult = Math.floor( Math.random() * 10 );
+    //If the value in randomResult is between 0 and 3, inclusive
+    //    return "rock"
+    if (randomResult >= 0 && randomResult <= 3) {
+        return "rock";
+    } 
+    //Else if the value in randomResult is between 4 and 6, inclusive
+    //    return "paper"
+    else if (randomResult >= 4 && randomResult <= 6) {
+        return "paper";
+    }
+    //Else if the value in randomResult is between 7 and 9, inclusive
+    //    return "scissors"
+    else if (randomResult >= 7 && randomResult <= 9) {
+        return "scissors";
+    }
+}
